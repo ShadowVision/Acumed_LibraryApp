7dead0a20471d1508225ca05fb72f3563d0ec8be
Tyler Poole <tpoole@pdx.edu>
2014-07-07 16:03:32 -0700

    Fixes for gold bar, magnifying glass, image size, etc.

--- www/js/asideHeight.js
+++ www/js/asideHeight.js
@@ -1,41 +1,9 @@
-function calcAsideBar() {
-    var titleEle = document.getElementById("title");
-    var viewPortHeight = document.getElementById("title").clientHeight;
-
-    var scrollTop = (titleEle.pageYOffset !== undefined) ? titleEle.pageYOffset : (titleEle).scrollTop;
-    document.getElementById("asideBottom").style.top = (scrollTop + viewPortHeight - 4) +"px";
-}
-
 function asideHeight () {
-    var titleScrollHeight;
-    var asHeight;
-    var asideEle;
-    var positionAsideBottom;
-
-    asideEle = document.getElementById("asTitle");
-    titleScrollHeight = document.getElementById("title").clientHeight;
-    asHeight = document.getElementById("asTitle").scrollHeight;
-    if(asHeight <= titleScrollHeight) {
-        asideEle.style.height = "100%";
-    }
-    else {
-        asideEle.style.height = "auto";
-    }
-
-
-    positionAsideBottom = document.getElementById("asideBottom");
-    positionAsideBottom.style.top = document.getElementById("title").clientHeight - 4 +"px";
-    positionAsideBottom.style.width = "40%";
-    positionAsideBottom.style.right = "0px";
 
     var screenWidth = window.innerWidth;
     var screenHeight = window.innerHeight;
     var sectionText = document.getElementById("sectionText");
     var sectionImage = document.getElementById("sectionId");
-    if(document.getElementById("polAsideImg"))
-    {
-        asideEle.style.height = "auto";
-    }
     if(screenWidth > screenHeight)
     {
         if(screenWidth >= 1600)
@@ -53,5 +21,25 @@ function asideHeight () {
     {
         sectionImage.style.width = "100%";
         sectionText.style.width = "100%";
+    }
+    var viewPortHeight;
+    var asHeight;
+    var asideEle;
+    asideEle = document.getElementById("asTitle");
+    viewPortHeight = document.getElementById("title").offsetHeight;
+    log("viewPortHeight: " + viewPortHeight);
+    asHeight = document.getElementById("asTitle").scrollHeight;
+    log("Aside scrollHeight: " + asHeight);
+    if(asHeight <= viewPortHeight) {
+        asideEle.style.height = "100%";
+    }
+    else {
+        asideEle.style.height = "auto";
+    }
+
+
+    if(document.getElementById("polAsideImg"))
+    {
+        asideEle.style.height = "auto";
     }
 }
